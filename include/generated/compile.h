b9fca30ce82948307d5068c4b4588d5f17c8416a
Rajan Verma <rajanverma747@gmail.com>
2019-02-06 20:50:13 -0800

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 9 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#9 SMP Wed Jan 23 21:42:08 PST 2019"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 7.3.1 20180425 [linaro-7.3-2018.05 revision d29120a424ecfbc167ef90065c0eeb7f91977701] (Linaro GCC 7.3-2018.05)"
